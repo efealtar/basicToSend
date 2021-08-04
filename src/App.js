f90e6809450db9acf559fd65b41444f13a94932d
efe <efetilu@gmail.com>
2021-08-04 14:54:41 +0300

    u

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React, { useState } from 'react';
+export default function App() {
+const [username, setUsername] = useState("")
+
+  return (
+    <form>
+      <h1>hellos {username}</h1>
+      <p>Enter Names</p>
+      <input type="text" onChange={e => setUsername(e.target.value)}></input>
+    </form>
+  )
+}
+
+
